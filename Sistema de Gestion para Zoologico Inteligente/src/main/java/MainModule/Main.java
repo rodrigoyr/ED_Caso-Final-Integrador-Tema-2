937ce53f605b3ea52455c3c5335284f34bd506bd
rodrigoyr <ryeperub@myuax.com>
2024-03-11 13:46:24 +0100

    fix commit

--- Sistema de Gestion para Zoologico Inteligente/src/main/java/MainModule/Main.java
+++ Sistema de Gestion para Zoologico Inteligente/src/main/java/MainModule/Main.java
@@ -1,14 +1,29 @@
-package modules;
-
-import modules.VisitantesModule.Trabajador;
-import modules.VisitantesModule.Visitante;
-import modules.VisitantesModule.QuioscoInteractivo;
-import modules.MantenimientoySeguridadModule.MantenimientoYseguridad;
-import modules.RecursosModule.Recursos;
-
-import java.util.Scanner;
-
 public class Main {
+
+    public interface GuiaVirtual {
+        void proporcionarTour();
+    }
+
+    public static class Trabajador implements GuiaVirtual {
+        @Override
+        public void proporcionarTour() {
+            System.out.println("Proporcionando un tour como trabajador.");
+        }
+    }
+
+    public static class Visitante implements GuiaVirtual {
+        @Override
+        public void proporcionarTour() {
+            System.out.println("Tomando un tour como visitante.");
+        }
+    }
+
+    public static class QuioscoInteractivo {
+        public void proporcionarInformacion(String tipo) {
+            System.out.println("Proporcionando información sobre " + tipo + ".");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -16,58 +31,23 @@ public class Main {
         String userType = scanner.nextLine();
 
         GuiaVirtual user;
-        MantenimientoYseguridad mantenimiento = new MantenimientoYseguridad("Instalación 1");
-        Recursos recursos = new Recursos();
+        QuioscoInteractivo quiosco = new QuioscoInteractivo();
 
         if (userType.equalsIgnoreCase("trabajador")) {
             user = new Trabajador();
-            System.out.println("1. Proporcionar un tour.");
-            System.out.println("2. Programar mantenimiento.");
-            System.out.println("3. Realizar reparación urgente.");
-            System.out.println("4. Agregar recursos.");
-            System.out.println("5. Realizar pedido de recursos.");
-            int opcion = scanner.nextInt();
-            switch (opcion) {
-                case 1:
-                    user.proporcionarTour();
-                    break;
-                case 2:
-                    mantenimiento.programarMantenimiento();
-                    break;
-                case 3:
-                    mantenimiento.reparacionUrgente();
-                    break;
-                case 4:
-                    recursos.agregarAlimento("Manzanas", 100, "Proveedor 1");
-                    recursos.agregarMedicina("Paracetamol", 50, "Proveedor 2");
-                    recursos.agregarEquipamiento("Jaulas", 10, "Proveedor 3");
-                    break;
-                case 5:
-                    recursos.realizarPedidoAlimentos("Manzanas", 50);
-                    recursos.realizarPedidoMedicinas("Paracetamol", 20);
-                    recursos.realizarPedidoEquipamiento("Jaulas", 5);
-                    break;
-                default:
-                    System.out.println("Opción no reconocida.");
-            }
+            user.proporcionarTour();
         } else if (userType.equalsIgnoreCase("visitante")) {
             user = new Visitante();
-            System.out.println("1. Tomar un tour.");
-            System.out.println("2. Visitar el Quiosco Interactivo.");
-            int opcion = scanner.nextInt();
-            switch (opcion) {
-                case 1:
-                    user.proporcionarTour();
-                    break;
-                case 2:
-                    QuioscoInteractivo quiosco = new QuioscoInteractivo();
-                    quiosco.proporcionarInformacion("animales");
-                    break;
-                default:
-                    System.out.println("Opción no reconocida.");
+            user.proporcionarTour();
+            System.out.println("¿Deseas visitar el Quiosco Interactivo? (sí/no)");
+            String respuesta = scanner.nextLine();
+            if (respuesta.equalsIgnoreCase("sí")) {
+                quiosco.proporcionarInformacion("animales");
             }
         } else {
             System.out.println("Tipo de usuario no reconocido. Por favor, ingresa 'trabajador' o 'visitante'.");
         }
+
+        scanner.close();
     }
 }
--- Sistema de Gestion para Zoologico Inteligente/src/modules/VisitantesModule/Visitantes.java
+++ Sistema de Gestion para Zoologico Inteligente/src/modules/VisitantesModule/Visitantes.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Visitantes {
 
     public interface GuiaVirtual {
         void proporcionarTour();
