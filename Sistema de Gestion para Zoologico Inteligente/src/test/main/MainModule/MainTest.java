32dad5c50cef3de0112525fb934e24601965fc1e
rodrigoyr <ryeperub@myuax.com>
2024-03-11 13:59:11 +0100

    fix commit

--- Sistema de Gestion para Zoologico Inteligente/src/test/main/MainModule/MainTest.java
+++ Sistema de Gestion para Zoologico Inteligente/src/test/main/MainModule/MainTest.java
@@ -6,6 +6,7 @@ import main.java.MainModule.Main;
 
 public class MainTest {
 
+    @Test
     public void testMain() {
         // Aquí puedes agregar las pruebas para tu método main
         // Por ejemplo, puedes probar que el método main no lanza ninguna excepción
