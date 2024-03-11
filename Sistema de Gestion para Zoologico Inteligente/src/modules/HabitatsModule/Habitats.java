21c1e6bd7fe7fe7b55b4aebdfcd27a6b3225a085
rodrigoyr <ryeperub@myuax.com>
2024-03-11 12:34:01 +0100

    animales commit

--- Sistema de Gestion para Zoologico Inteligente/src/modules/HabitatsModule/Habitats.java
+++ Sistema de Gestion para Zoologico Inteligente/src/modules/HabitatsModule/Habitats.java
@@ -1,4 +1,63 @@
-package modules.HabitatsModule;
+// En modules/habitats
+package modules.habitats;
 
-public class Habitats {
+public abstract class Habitat {
+    private String tipo;
+    private int capacidad;
+
+    public Habitat(String tipo, int capacidad) {
+        this.tipo = tipo;
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public abstract void monitorear();
+    // Otros métodos específicos de los hábitats...
+}
+
+// En modules/habitats
+package modules.habitats;
+
+public class Acuatico extends Habitat {
+    private String tipoAgua;
+
+    public Acuatico(String tipo, int capacidad, String tipoAgua) {
+        super(tipo, capacidad);
+        this.tipoAgua = tipoAgua;
+    }
+
+    @Override
+    public void monitorear() {
+        System.out.println("Monitoreando hábitat acuático: " + getTipo() +
+                ", Capacidad: " + getCapacidad() +
+                ", Tipo de Agua: " + tipoAgua);
+        // Lógica adicional de monitoreo para hábitats acuáticos...
+    }
+}
+
+// En modules/habitats
+package modules.habitats;
+
+public class Terrestre extends Habitat {
+    private String tipoTerreno;
+
+    public Terrestre(String tipo, int capacidad, String tipoTerreno) {
+        super(tipo, capacidad);
+        this.tipoTerreno = tipoTerreno;
+    }
+
+    @Override
+    public void monitorear() {
+        System.out.println("Monitoreando hábitat terrestre: " + getTipo() +
+                ", Capacidad: " + getCapacidad() +
+                ", Tipo de Terreno: " + tipoTerreno);
+        // Lógica adicional de monitoreo para hábitats terrestres...
+    }
 }
