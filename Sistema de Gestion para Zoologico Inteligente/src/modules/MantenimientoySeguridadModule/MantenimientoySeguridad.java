0543133519c877ad2706c786fe1426ed019ddb95
rodrigoyr <ryeperub@myuax.com>
2024-03-11 12:35:48 +0100

    mantenimiento y seguridad commit

--- Sistema de Gestion para Zoologico Inteligente/src/modules/MantenimientoySeguridadModule/MantenimientoySeguridad.java
+++ Sistema de Gestion para Zoologico Inteligente/src/modules/MantenimientoySeguridadModule/MantenimientoySeguridad.java
@@ -1,4 +1,34 @@
-package modules.MantenimientoySeguridadModule;
+// En modules
+package modules;
 
-public class MantenimientoySeguridad {
+public class MantenimientoYseguridad {
+    private String instalacion;
+    private boolean mantenimientoProgramado;
+    private boolean reparacionUrgente;
+
+    public MantenimientoYseguridad(String instalacion) {
+        this.instalacion = instalacion;
+        this.mantenimientoProgramado = false;
+        this.reparacionUrgente = false;
+    }
+
+    public void programarMantenimiento() {
+        System.out.println("Mantenimiento programado para la instalación: " + instalacion);
+        mantenimientoProgramado = true;
+        reparacionUrgente = false;
+    }
+
+    public void reparacionUrgente() {
+        System.out.println("Reparación urgente en la instalación: " + instalacion);
+        reparacionUrgente = true;
+        mantenimientoProgramado = false;
+    }
+
+    public boolean isMantenimientoProgramado() {
+        return mantenimientoProgramado;
+    }
+
+    public boolean isReparacionUrgente() {
+        return reparacionUrgente;
+    }
 }
