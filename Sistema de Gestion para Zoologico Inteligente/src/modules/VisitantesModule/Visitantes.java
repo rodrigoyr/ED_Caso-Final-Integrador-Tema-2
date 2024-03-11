bf9d690f52021640ca0026b488c9dd1150032129
rodrigoyr <ryeperub@myuax.com>
2024-03-11 12:38:45 +0100

    visitantes commit

--- Sistema de Gestion para Zoologico Inteligente/src/modules/VisitantesModule/Visitantes.java
+++ Sistema de Gestion para Zoologico Inteligente/src/modules/VisitantesModule/Visitantes.java
@@ -1,4 +1,59 @@
-package modules.VisitantesModule;
+// En modules
+package modules;
 
-public class Visitantes {
+public interface GuiaVirtual {
+    void proporcionarTour();
+}
+
+// En modules
+package modules;
+
+public class GuiaNinos implements GuiaVirtual {
+    @Override
+    public void proporcionarTour() {
+        System.out.println("Bienvenido, pequeño explorador. Te llevaré en un tour especial para niños.");
+        // Lógica específica para el tour de niños...
+    }
+}
+
+// En modules
+package modules;
+
+public class GuiaAves implements GuiaVirtual {
+    @Override
+    public void proporcionarTour() {
+        System.out.println("¡Hola amante de las aves! Vamos a explorar el fascinante mundo de nuestras aves.");
+        // Lógica específica para el tour de amantes de aves...
+    }
+}
+
+// En modules
+package modules;
+
+public class GuiaMamiferos implements GuiaVirtual {
+    @Override
+    public void proporcionarTour() {
+        System.out.println("¡Prepárate para un emocionante viaje entre los mamíferos! ¡Vamos a comenzar!");
+        // Lógica específica para el tour de amantes de mamíferos...
+    }
+}
+
+// En modules
+package modules;
+
+public class QuioscoInteractivo {
+    public void proporcionarInformacion(String categoria) {
+        switch (categoria.toLowerCase()) {
+            case "animales":
+                System.out.println("¡Bienvenido al Quiosco Interactivo de Animales! Aquí encontrarás información sobre nuestros maravillosos habitantes.");
+                // Lógica específica para la información de animales...
+                break;
+            case "habitats":
+                System.out.println("Explora el Quiosco Interactivo de Habitats para conocer más sobre los entornos de nuestros animales.");
+                // Lógica específica para la información de hábitats...
+                break;
+            default:
+                System.out.println("Lo siento, no se encuentra disponible la información para la categoría seleccionada.");
+        }
+    }
 }
