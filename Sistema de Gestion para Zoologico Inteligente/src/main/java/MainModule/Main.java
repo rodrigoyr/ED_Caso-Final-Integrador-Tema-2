0ab8a5a1db2a2d6c63b48eae170f5ff8d7f45e1e
rodrigoyr <ryeperub@myuax.com>
2024-03-11 14:11:17 +0100

    test commit

--- Sistema de Gestion para Zoologico Inteligente/src/main/java/MainModule/Main.java
+++ Sistema de Gestion para Zoologico Inteligente/src/main/java/MainModule/Main.java
@@ -1,5 +1,12 @@
 package main.java.MainModule;
 import java.util.Scanner;
+import main.java.AnimalesModulemain
+        .Ave;
+import main.java.AnimalesModule.Mamifero;
+import main.java.VisitantesModule.GuiaNinos;
+import main.java.VisitantesModule.GuiaAves;
+import main.java.VisitantesModule.GuiaMamiferos;
+import main.java.VisitantesModule.QuioscoInteractivo;
 
 public class Main {
 
@@ -12,6 +19,16 @@ public class Main {
         public void proporcionarTour() {
             System.out.println("Proporcionando un tour como trabajador.");
         }
+
+        public void realizarTarea(Object tarea) {
+            if (tarea instanceof Ave) {
+                ((Ave) tarea).alimentar();
+            } else if (tarea instanceof Mamifero) {
+                ((Mamifero) tarea).alimentar();
+            } else {
+                System.out.println("Tarea no reconocida.");
+            }
+        }
     }
 
     public static class Visitante implements GuiaVirtual {
@@ -19,38 +36,70 @@ public class Main {
         public void proporcionarTour() {
             System.out.println("Tomando un tour como visitante.");
         }
-    }
 
-    public static class QuioscoInteractivo {
-        public void proporcionarInformacion(String tipo) {
-            System.out.println("Proporcionando información sobre " + tipo + ".");
+        public void realizarTarea(Object tarea) {
+            if (tarea instanceof GuiaNinos) {
+                ((GuiaNinos) tarea).proporcionarTour();
+            } else if (tarea instanceof GuiaAves) {
+                ((GuiaAves) tarea).proporcionarTour();
+            } else if (tarea instanceof GuiaMamiferos) {
+                ((GuiaMamiferos) tarea).proporcionarTour();
+            } else if (tarea instanceof QuioscoInteractivo) {
+                ((QuioscoInteractivo) tarea).proporcionarInformacion("animales");
+            } else {
+                System.out.println("Tarea no reconocida.");
+            }
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Bienvenido al sistema. ¿Eres un trabajador o un visitante?");
-        String userType = scanner.nextLine();
-
         GuiaVirtual user;
-        QuioscoInteractivo quiosco = new QuioscoInteractivo();
+        Ave ave = new Ave("Pájaro", 2, "Plumas coloridas");
+        Mamifero mamifero = new Mamifero("Elefante", 10, "Piel gruesa");
+        GuiaNinos guiaNinos = new GuiaNinos();
+        GuiaAves guiaAves = new GuiaAves();
+        GuiaMamiferos guiaMamiferos = new GuiaMamiferos();
+        QuioscoInteractivo quioscoInteractivo = new QuioscoInteractivo();
 
-        if (userType.equalsIgnoreCase("trabajador")) {
-            user = new Trabajador();
-            user.proporcionarTour();
-        } else if (userType.equalsIgnoreCase("visitante")) {
-            user = new Visitante();
-            user.proporcionarTour();
-            System.out.println("¿Deseas visitar el Quiosco Interactivo? (sí/no)");
-            String respuesta = scanner.nextLine();
-            if (respuesta.equalsIgnoreCase("sí")) {
-                quiosco.proporcionarInformacion("animales");
+        while (true) {
+            System.out.println("Bienvenido al sistema. ¿Eres un 1. Trabajador o 2. Visitante?");
+            int userType = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            if (userType == 1) {
+                user = new Trabajador();
+                user.proporcionarTour();
+                System.out.println("¿Qué tarea deseas realizar? 1. Alimentar ave, 2. Alimentar mamífero");
+                int tarea = scanner.nextInt();
+                scanner.nextLine(); // consume newline
+                if (tarea == 1) {
+                    ((Trabajador) user).realizarTarea(ave);
+                } else if (tarea == 2) {
+                    ((Trabajador) user).realizarTarea(mamifero);
+                } else {
+                    System.out.println("Opción no reconocida. Por favor, ingresa 1 o 2.");
+                }
+            } else if (userType == 2) {
+                user = new Visitante();
+                user.proporcionarTour();
+                System.out.println("¿Qué deseas hacer? 1. Tour para niños, 2. Tour de aves, 3. Tour de mamíferos, 4. Visitar quiosco interactivo");
+                int opcion = scanner.nextInt();
+                scanner.nextLine(); // consume newline
+                if (opcion == 1) {
+                    ((Visitante) user).realizarTarea(guiaNinos);
+                } else if (opcion == 2) {
+                    ((Visitante) user).realizarTarea(guiaAves);
+                } else if (opcion == 3) {
+                    ((Visitante) user).realizarTarea(guiaMamiferos);
+                } else if (opcion == 4) {
+                    ((Visitante) user).realizarTarea(quioscoInteractivo);
+                } else {
+                    System.out.println("Opción no reconocida. Por favor, ingresa 1, 2, 3 o 4.");
+                }
+            } else {
+                System.out.println("Tipo de usuario no reconocido. Por favor, ingresa 1 o 2.");
             }
-        } else {
-            System.out.println("Tipo de usuario no reconocido. Por favor, ingresa 'trabajador' o 'visitante'.");
         }
-
-        scanner.close();
     }
 }
