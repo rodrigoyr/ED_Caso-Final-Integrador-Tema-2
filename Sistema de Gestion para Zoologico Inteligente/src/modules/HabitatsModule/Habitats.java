4cb9371b6e88db52c34ea33f69d4f46841e62bce
rodrigoyr <ryeperub@myuax.com>
2024-03-11 20:02:36 +0100

    main commit

--- Sistema de Gestion para Zoologico Inteligente/src/modules/HabitatsModule/Habitats.java
+++ Sistema de Gestion para Zoologico Inteligente/src/modules/HabitatsModule/Habitats.java
@@ -1,57 +1,120 @@
-// En modules/HabitatsModule
-package modules.HabitatsModule;
+// En src/main/java/MainModule/Main.java
+package MainModule;
 
-public abstract class Habitats {
-    private String tipo;
-    private int capacidad;
+import modules.AnimalesModule.Ave;
+import modules.AnimalesModule.Mamifero;
+import modules.HabitatsModule.Acuatico;
+import modules.HabitatsModule.Terrestre;
+import modules.MantenimientoySeguridadModule.MantenimientoYseguridad;
+import modules.RecursosModule.Recursos;
+import modules.VisitantesModule.Visitantes.QuioscoInteractivo;
 
-    public Habitats(String tipo, int capacidad) {
-        this.tipo = tipo;
-        this.capacidad = capacidad;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Bienvenido al Sistema de Gestión para Zoológico Inteligente");
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Trabajador");
+            System.out.println("2. Visitante");
+            System.out.println("3. Salir");
+
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    menuTrabajador();
+                    break;
+                case 2:
+                    menuVisitante();
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa. ¡Hasta luego!");
+                    System.exit(0);
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione nuevamente.");
+            }
+        }
     }
 
-    public String getTipo() {
-        return tipo;
+    private static void menuTrabajador() {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Menú Trabajador");
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Dar de comer a los animales");
+            System.out.println("2. Limpiar los hábitats");
+            System.out.println("3. Reponer recursos");
+            System.out.println("4. Programar mantenimiento");
+            System.out.println("5. Reparación urgente");
+            System.out.println("6. Volver al menú principal");
+
+            int opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    Ave ave = new Ave("EjemploAve", 5, "TipoPlumajeEjemplo");
+                    Mamifero mamifero = new Mamifero("EjemploMamifero", 3, "TipoPelajeEjemplo");
+                    // Lógica para dar de comer a los animales
+                    break;
+                case 2:
+                    Acuatico acuatico = new Acuatico("HabitatAcuatico", 10, "TipoAguaEjemplo");
+                    Terrestre terrestre = new Terrestre("HabitatTerrestre", 15, "TipoTerrenoEjemplo");
+                    // Lógica para limpiar los hábitats
+                    break;
+                case 3:
+                    Recursos recursos = new Recursos();
+                    // Lógica para reponer recursos
+                    break;
+                case 4:
+                    MantenimientoYseguridad mantenimientoYseguridad = new MantenimientoYseguridad("InstalacionEjemplo");
+                    // Lógica para programar mantenimiento
+                    break;
+                case 5:
+                    MantenimientoYseguridad reparacionUrgente = new MantenimientoYseguridad("InstalacionEjemplo");
+                    // Lógica para reparación urgente
+                    break;
+                case 6:
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione nuevamente.");
+            }
+        }
     }
 
-    public int getCapacidad() {
-        return capacidad;
-    }
+    private static void menuVisitante() {
+        Scanner scanner = new Scanner(System.in);
 
-    public abstract void monitorear();
-    // Otros métodos específicos de los hábitats...
-}
+        while (true) {
+            System.out.println("Menú Visitante");
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Visitar hábitats");
+            System.out.println("2. Tour específico para niños");
+            System.out.println("3. Visitar quiosco con información");
+            System.out.println("4. Volver al menú principal");
 
-public class Acuatico extends Habitats {
-    private String tipoAgua;
+            int opcion = scanner.nextInt();
 
-    public Acuatico(String tipo, int capacidad, String tipoAgua) {
-        super(tipo, capacidad);
-        this.tipoAgua = tipoAgua;
-    }
-
-    @Override
-    public void monitorear() {
-        System.out.println("Monitoreando hábitat acuático: " + getTipo() +
-                ", Capacidad: " + getCapacidad() +
-                ", Tipo de Agua: " + tipoAgua);
-        // Lógica adicional de monitoreo para hábitats acuáticos...
-    }
-}
-
-public class Terrestre extends Habitats {
-    private String tipoTerreno;
-
-    public Terrestre(String tipo, int capacidad, String tipoTerreno) {
-        super(tipo, capacidad);
-        this.tipoTerreno = tipoTerreno;
-    }
-
-    @Override
-    public void monitorear() {
-        System.out.println("Monitoreando hábitat terrestre: " + getTipo() +
-                ", Capacidad: " + getCapacidad() +
-                ", Tipo de Terreno: " + tipoTerreno);
-        // Lógica adicional de monitoreo para hábitats terrestres...
+            switch (opcion) {
+                case 1:
+                    // Lógica para visitar hábitats
+                    break;
+                case 2:
+                    // Lógica para tour específico para niños
+                    break;
+                case 3:
+                    QuioscoInteractivo quioscoInteractivo = new QuioscoInteractivo();
+                    // Lógica para visitar quiosco con información
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione nuevamente.");
+            }
+        }
     }
 }
